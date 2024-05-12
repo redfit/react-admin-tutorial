69f8a3842b493d32d5b822a65da944d09ab4b748
redfit <nagita.0724.0812@gmail.com>
2024-05-12 20:09:52 +0900

    Customizing The Page Title

--- src/posts.tsx
+++ src/posts.tsx
@@ -9,6 +9,7 @@ import {
   ReferenceInput,
   SimpleForm,
   TextInput,
+  useRecordContext,
 } from "react-admin";
 
 export const PostList = () => (
@@ -22,8 +23,13 @@ export const PostList = () => (
   </List>
 );
 
+const PostTitle = () => {
+  const record = useRecordContext();
+  return <span>Post {record ? `${record.title}` : ""}</span>;
+};
+
 export const PostEdit = () => (
-  <Edit>
+  <Edit title={<PostTitle />}>
     <SimpleForm>
       <TextInput source="id" InputProps={{ disabled: true }} />
       <ReferenceInput source="userId" reference="users" />
