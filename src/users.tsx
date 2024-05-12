ceb7e2c062a454634ee75f0b73c59186e67f3fa3
redfit <nagita.0724.0812@gmail.com>
2024-05-12 19:31:15 +0900

    Writing A Custom Field

--- src/users.tsx
+++ src/users.tsx
@@ -1,5 +1,6 @@
 import { useMediaQuery, Theme } from "@mui/material";
 import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
+import MyUrlField from "./MyUrlField";
 
 export const UserList = () => {
   const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
@@ -17,7 +18,7 @@ export const UserList = () => {
           <TextField source="name" />
           <EmailField source="email" />
           <TextField source="phone" />
-          <TextField source="website" />
+          <MyUrlField source="website" />
           <TextField source="company.name" />
         </Datagrid>
       )}
